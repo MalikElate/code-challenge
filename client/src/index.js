8dc7189ac74e33d0b57cc2c21eb3e72d483603da
Malik Elate <Malik.e1955@gmail.com>
2021-02-21 12:54:03 -0600

    added redux

--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './Components/App/App.jsx'; 
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import createSagaMiddleware from 'redux-saga';
+import logger from 'redux-logger';
+import rootReducer from './Redux/Reducers/root.reducer';
+import rootSaga from './Redux/Sagas/root.saga'; 
+import App from './Components/App/App'; 
 
+const sagaMiddleware = createSagaMiddleware();
+
+// we don't want a whole ton of console logs in our production code
+// logger will only be added to your project if your in development mode
+const middlewareList = process.env.NODE_ENV === 'development' ?
+  [sagaMiddleware, logger] :
+  [sagaMiddleware];
+
+const store = createStore(
+  rootReducer,
+  applyMiddleware(...middlewareList),
+);
+
+sagaMiddleware.run(rootSaga);
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </Provider>,
+  document.getElementById('root'),
 );
 
